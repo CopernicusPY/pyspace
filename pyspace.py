732408ec17d9805c2253c7ae8bea56b8590fffbe
Capernicus <80885284+CapernicusPY@users.noreply.github.com>
2022-02-19 16:53:03 +0200

    Added mars_picture method.
    mars_picture allowes the user to retrieve a list of dictionaries corresponding to the specified arguments. You can specify the rover wich the photo is from, the camera, the sol date and the earth date of the picture. Results are returned in "pages" (25 items per page)

--- pyspace.py
+++ pyspace.py
@@ -85,3 +85,69 @@ class PySpace:
             print(colorama.Fore.GREEN + f"[INFO] Request completed\n[INFO] Status Code: {resp.status_code}\n[INFO] Response:\n{colorama.Fore.WHITE + resp.text}")
             return resp.json()
     
+    def mars_picture(self,rover:str="Curiosity", sol=None,earth_date:str=None, camera:str="all", page=1):
+        """
+        Returns data for the Mars Picture that matches the given data.
+
+        Parameters
+        ==========
+        rover: string, default -> "Curiosity"
+            Rover name to access the database
+        sol: integer, default -> None
+            Martian date that corresponds to when the image got captured. 
+        earth_date: string, default -> None 
+            Earth date that corresponds to when the image got captured
+        camera: string, default -> "all"
+            Return results from specific camera on passed Rover
+        page: integer, default -> 1
+            How many pages of results to return. (25 items per page)
+
+        Raises
+        ======
+        TypeError
+            Raised if both sol and earth_date are specified.
+        ValueError
+            Raised if rover specified is not one of Curiosity Opportunity Spirit or Perseverance
+        HTTPError
+            Raised if response fails. (Status code != 200)
+
+        Returns
+        =======
+        list 
+            List containing dictionaries with the JSON data from the Mars Rover API
+        """
+        
+        params =  {
+            "api_key":self.api_key,
+            "page":page
+        }
+        if rover.lower() not in ('curiosity', 'opportunity', 'spirit', 'perseverance'):
+            raise ValueError(colorama.Fore.RED+"<rover> must be one of the following: Curiosity, Opportunity, Spirit, Perseverance")
+        if earth_date != None and sol != None:
+            raise TypeError(colorama.Fore.RED+"Bad combination. Either <earth_date> or <sol> can be specified")
+        if camera != "all":
+            params['camera'] = camera
+        if sol != None:
+            params['sol'] = sol
+        if earth_date != None:
+            if not isinstance(earth_date, (str, datetime.datetime)):
+                raise TypeError("Earth date must be in YYYY-MM-DD format.")
+            elif isinstance(earth_date, datetime.datetime):
+                earth_date = earth_date.strftime("%Y-%m-%d")
+            params['earth_date'] = earth_date
+
+        resp = requests.get(f"https://api.nasa.gov/mars-photos/api/v1/rovers/{rover}/photos", params=params)
+
+        if resp.status_code != 200:
+            print(colorama.Fore.RED+f"[Error] Status Code: {resp.status_code} ({resp.reason})\n[ERROR] Response: {resp.text}")
+            raise requests.exceptions.HTTPError(resp.reason)
+        else:
+            self.limit_remaining = resp.headers['X-RateLimit-Remaining']
+            print(colorama.Fore.GREEN + f"[INFO] Request completed\n[INFO] Status Code: {resp.status_code}\n[INFO] Response:\n{colorama.Fore.WHITE + resp.text}")
+            return resp.json()['photos']
+#Tests 
+
+my_apod = PySpace() #Initialize with DEMO_KEY
+my_apod.mars_picture(rover="spirit",sol=1000)
+# my_apod.picture_of_the_day(date="2022-02-05")
+print(my_apod.limit_remaining)
