15c0f40a9c930905d852477883564f473fc0641f
Capernicus <80885284+CapernicusPY@users.noreply.github.com>
2022-03-09 13:40:33 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,19 +12,13 @@ author = 'Capernicus'
 release = '1.0'
 version = '1.5.0'
 
-# -- General configuration
-autosectionlabel_prefix_document = True
-hoverxref_auto_ref = True
-hoverxref_sphinxtabs = True
-
 extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
     'karma_sphinx_theme',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    'autoapi.extension'
 
 ]
 
