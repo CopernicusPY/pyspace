baa64aa4a8c8e066744d76fbcd782b581c41e4fb
Capernicus <80885284+CapernicusPY@users.noreply.github.com>
2022-03-23 20:26:03 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,7 +3,7 @@
 import os 
 import sys
 
-sys.path.insert(0, os.path.abspath('../../..'))
+sys.path.insert(0, os.path.abspath('../../'))
 
 # -- AutoAPI configuration settings
 # autoapi_dirs = ['../../']
@@ -15,13 +15,13 @@ author = 'Capernicus'
 
 release = '1.0'
 version = '1.5.0'
-
+#Enable hoverxref 
+hoverxref_sphinxtabs = True
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'karma_sphinx_theme',
     'sphinx.ext.napoleon',
+    'hoverxref.extension'
 #     'autoapi.extension'
 
 ]
