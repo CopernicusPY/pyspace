acf400f7e573ec23b94e663a4576f25ea3d32c4b
Capernicus <80885284+CapernicusPY@users.noreply.github.com>
2022-03-23 20:32:19 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,7 +5,7 @@ import sys
 
 #For real idk what the fuck is going on im just trying wrong paths since nothing does work
 
-sys.path.insert(0, os.path.abspath('../../..')
+sys.path.insert(0, os.path.abspath('../../..'))
 
 # -- AutoAPI configuration settings
 # autoapi_dirs = ['../../']
