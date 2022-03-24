983f185ccaaaad9f4425f6419cd8029cd664706b
Capernicus <80885284+CapernicusPY@users.noreply.github.com>
2022-03-24 13:16:18 +0200

    Add 404 page

--- iss_track/app.py
+++ iss_track/app.py
@@ -5,4 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 def iss_track():
     return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("errors/404.html"), 404
+# @app.errorhandler(500)
+# def page_not_found(e):
+#     return render_template("errors/500.html"), 500
 app.run(debug=True)
